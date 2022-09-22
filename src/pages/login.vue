37d32f3655b97f3569bb19871180d52c59b19f5e
zhengjun.zuo <zhengjun.zuo@meditrusthealth.com>
2022-09-22 18:32:11 +0800

    第二天学习内容

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,35 +1,42 @@
 <template>
-  <el-row class="min-h-screen bg-indigo-500">
-    <el-col :span="16" class="flex items-center justify-center">
+  <el-row class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
-        <div class="text-gray-200 text-sm">此站点是《vue3 + vite实战商城后台开发》视频课程的演示地址</div>
+        <div>欢迎光临</div>
+        <div>此站点是《vue3 + vite实战商城后台开发》视频课程的演示地址</div>
       </div>
     </el-col>
-    <el-col :span="8"  class="bg-light-50 flex items-center justify-center flex-col">
-      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <h2 class="title">欢迎回来</h2>
+      <div>
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
-      <el-form :model="form" class="w-[250px]">
-        <el-form-item>
+      <el-form ref="formRef" :rules="rules" :model="form" class="w-[250px]">
+        <el-form-item prop="username">
           <el-input v-model="form.username" placeholder="请输入用户名">
             <template #prefix>
-              <el-icon><user /></el-icon>
+              <el-icon>
+                <user/>
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.password" placeholder="请输入密码">
+        <el-form-item prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码" show-password
+                    @keyup.enter.native="onSubmit">
             <template #prefix>
-              <el-icon><lock /></el-icon>
+              <el-icon>
+                <lock/>
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit">登 录</el-button>
+          <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit" :loading="loading"
+          >登 录
+          </el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -37,17 +44,98 @@
 </template>
 
 <script setup>
-import {reactive} from "vue";
+import {ref, reactive} from 'vue'
+import {useRouter} from 'vue-router'
+import {login, getInfo} from '~/api/manager'
+import {setToken} from "~/composables/auth.js";
+import {toast} from "~/composables/util.js";
 
+
+const router = useRouter()
+const loading = ref(false)
+// do not use same name with ref
 const form = reactive({
   username: "",
   password: ""
 })
+
+const rules = {
+  username: [
+    {
+      required: true,
+      message: '用户名不能为空',
+      trigger: 'blur'
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: '用户名不能为空',
+      trigger: 'blur'
+    },
+  ]
+}
+
+const formRef = ref(null)
+
 const onSubmit = () => {
-  alert('submit')
+  formRef.value.validate((valid) => {
+    if (!valid) {
+      return false
+    }
+    loading.value = true
+    login(form.username, form.password).then(res => {
+      console.log(res)
+      // 提示成功
+
+      toast("登录成功")
+
+      // 存储token和用户相关信息，下节课讲
+      setToken(res.token)
+
+      // 获取用户相关信息
+      getInfo().then(res2 => {
+        console.log(res2)
+      })
+      // 跳转到后台首页
+      router.push("/")
+    }).finally(() => {
+      return loading.value = false
+    })
+  })
 }
 </script>
 
 <style scoped>
+.login-container {
+  @apply min-h-screen bg-indigo-500;
+}
 
+.login-container .left, .login-container .right {
+  @apply flex items-center justify-center;
+}
+
+.login-container .right {
+  @apply bg-light-50 flex-col;
+}
+
+.left > div > div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+
+.left > div > div:last-child {
+  @apply text-gray-200 text-sm;
+}
+
+.right .title {
+  @apply font-bold text-3xl text-gray-800;
+}
+
+.right > div {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+
+.right .line {
+  @apply h-[1px] w-16 bg-gray-200;
+}
 </style>
